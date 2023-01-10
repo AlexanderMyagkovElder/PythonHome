e32c90b664e63dcbe838dd5c6eefc415d87a5ac1
Alexander Myagkov <anistonbale@gmail.com>
2023-01-11 01:44:55 +0300

    Sem9

--- Sem9/HomeTsk.py
+++ Sem9/HomeTsk.py
@@ -1,1 +1,11 @@
-print('1')
+import requests
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+
+url = 'https://sazonovmebel.ru/'
+response = requests.get(url).text
+soup = bs(response, 'html.parser')
+find_text = soup.find('body').find('div', class_="container-list"). \
+    find(class_="element-content button-1 element-content--free"). \
+    find(class_="btn-content text-color hover-text-color btn-content-text icon-on-left")
+tqdm(print(find_text.text))
